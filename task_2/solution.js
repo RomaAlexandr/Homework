6bf50c55988463d1e1f75431e0629db8724a5141
RomaAlexandr <roma.afanas.alex@gmail,com>
2022-10-20 02:21:58 +0300

    task 2 complete

--- task_2/solution.js
+++ task_2/solution.js
@@ -4,6 +4,15 @@ function calcShipping(sum, min, shipping) {
     let shippingPrice = shipping; // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
+    let shippingSum;
+
+    if(productsSum == 0 || productsSum >= freeShippingMinSum) {
+      shippingSum = 0; 
+    } else { 
+      shippingSum = shippingPrice;
+    } 
+
+    
 
     // создайте переменную shippingSum
 
@@ -27,7 +36,13 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
+    let discountSum
 
+    if(productsSum >= discountMinSum) {
+        discountSum = productsSum / 100 * discountPart;
+    } else {
+        discountSum = 0;
+    }
     // создайте переменную discountSum
 
     // если productsSum больше или равно discountMinSum,
@@ -45,13 +60,18 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
+    
     // создайте переменную totalSum
-
+    let totalSum = productsSum;
+    totalSum = totalSum - discountSum;
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
-
+    totalSum = totalSum + shippingSum;
+    let freeShipping;
+    freeShipping = !shippingSum;
+   
     // прибавьте к totalSum значение shippingSum
 
     // создайте переменную freeShipping
